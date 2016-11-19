6e44527cb23815c1a6eb0df2d813620587dd1d1b
Aakash <Aakash@AAKASH>
2016-11-18 18:37:08 -0800

    Add project files.

---
+++ Comp229-Assign03/myPages/courseenrollment.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Comp229_Assign03.myPages
+{
+    public partial class courseenrollment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bindData();
+        }
+        protected void bindData()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Comp229Assign03ConnectionString"].ToString());
+
+
+            SqlCommand comm = new SqlCommand("SELECT * FROM Students", connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                GridViewCourse.DataSource = reader;
+                GridViewCourse.DataBind();
+                reader.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Comp229-Assign03/myPages/home.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Comp229_Assign03.myPages
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bindData();
+        }
+        protected void bindData()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Comp229Assign03ConnectionString"].ToString());
+
+            
+            SqlCommand comm = new SqlCommand("SELECT * FROM Students", connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                GridView.DataSource = reader;
+                GridView.DataBind();
+                reader.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        protected void addStudentButton(object sender, EventArgs e)
+        {
+            Response.Redirect(url: "~/myPages/addstudent.aspx");
+        }
+
+        protected void GridView_SelectedRow(object sender, EventArgs e)
+        {
+            GridViewRow row = GridView.SelectedRow; 
+            string StudentID = row.Cells[1].Text;
+            
+            Response.Redirect("~/myPages/student.aspx?ID=" + StudentID); // this will add the student ID 
+        }
+    }
+}
